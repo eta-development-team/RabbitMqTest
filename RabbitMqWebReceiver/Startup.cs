ebd348809ff17536c5b08edd2f66486387621d75
Leo <eta.development.leo@outlook.com>
2020-02-15 13:22:26 +0300

    Add SignarR to pass data from bus into web page

--- RabbitMqWebReceiver/Startup.cs
+++ RabbitMqWebReceiver/Startup.cs
@@ -18,7 +18,6 @@ namespace RabbitMqWebReceiver
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddHostedService<RabbitMqBusReceiver>();
             services.AddControllersWithViews();
             services.AddSignalR();
